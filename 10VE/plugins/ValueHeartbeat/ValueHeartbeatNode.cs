ebc088f0267811efee5cd50f9437399d81c9bd18
jens <jens@lea.io>
2015-04-29 18:29:42 +0200

    Adding Framerate parameter to Heartbeat statistics NODE

--- 10VE/plugins/ValueHeartbeat/ValueHeartbeatNode.cs
+++ 10VE/plugins/ValueHeartbeat/ValueHeartbeatNode.cs
@@ -20,7 +20,10 @@ namespace VVVV.Nodes
 		#region fields & pins
 		[Input("Input", DefaultValue = 1.0)]
 		public IDiffSpread<double> FInput;
-
+		
+		[Input("Framerate", IsSingle = true)]
+		public IDiffSpread<int> FFramerate;
+		
 		[Input("Reset", IsBang = true, IsSingle = true)]
 		public IDiffSpread<bool> FReset;
 		
@@ -40,15 +43,15 @@ namespace VVVV.Nodes
 		[Import()]
 		public ILogger FLogger;
 		#endregion fields & pins
-
+		
 		// these variables are volatile because they are used during the interrupt service routine!
 		int BPM;                   		// used to hold the pulse rate
-	 	double Signal;               	// holds the incoming raw data
+		double Signal;               	// holds the incoming raw data
 		ulong IBI = 600;            	// holds the time between beats, the Inter-Beat Interval
 		bool Pulse = false;     		// true when pulse wave is high, false when it's low
-		bool QS = false;        		// becomes true when finds a beat. 
-				
-	 	ulong[] rate = new ulong[10];   // used to hold last ten IBI values
+		bool QS = false;        		// becomes true when finds a beat.
+		
+		ulong[] rate = new ulong[10];   // used to hold last ten IBI values
 		ulong sampleCounter = 0;        // used to determine pulse timing, ulong
 		ulong lastBeatTime = 0;         // used to find the inter beat interval, ulong
 		double P =0.5;                  // used to find peak in pulse wave
@@ -56,7 +59,9 @@ namespace VVVV.Nodes
 		double thresh = 0.5;            // used to find instant moment of heart beat
 		double amp = 0.1;               // used to hold amplitude of pulse waveform
 		bool firstBeat = true;        	// used to seed rate array so we startup with reasonable BPM
-		bool secondBeat = true;       	// used to seed rate array so we startup with reasonable BPM 
+		bool secondBeat = true;       	// used to seed rate array so we startup with reasonable BPM
+		
+		uint frameRate = 167;
 		
 		//called when data for any output pin is requested
 		public void Evaluate(int SpreadMax)
@@ -66,120 +71,126 @@ namespace VVVV.Nodes
 			FBPM.SliceCount = SpreadMax;
 			FPeak.SliceCount = SpreadMax;
 			
+			if (FFramerate.IsChanged && FFramerate.SliceCount > 0 ) {
+				int _frameRate = FFramerate[0];
+				frameRate = _frameRate >= 0 ? (uint)_frameRate : 0;
+				frameRate = (uint)(1000/(float)frameRate);
+			}
+			
 			if(FReset.IsChanged && FReset[0])
 			{
 				thresh = 0.5;                          // set thresh default
 				P = 0.5;                               // set P default
 				T = 0.5;                               // set T default
 				amp = 0.1;
-				lastBeatTime = sampleCounter;          // bring the lastBeatTime up to date        
+				lastBeatTime = sampleCounter;          // bring the lastBeatTime up to date
 				firstBeat = true;                      // set these to avoid noise
 				secondBeat = true;
 			}
 			
-			if(FInput.IsChanged) 
+			if(FInput.IsChanged)
 			{
 				for (int i = 0; i < SpreadMax; i++) {
-				
-					sampleCounter += 167;                         // keep track of the time in mS with this variable
-	    			Signal = FInput[i];
+					
+					sampleCounter += frameRate;                         // keep track of the time in mS with this variable
+					Signal = FInput[i];
 					ulong N = ( sampleCounter - lastBeatTime);       // monitor the time since the last beat to avoid noise
 					FIBI[i] = IBI;
 					
 					//FLogger.Log(LogType.Debug, "N:" + N.ToString());
 					
 					//  find the peak and trough of the pulse wave
-	    			if( (Signal < thresh) && (N > (IBI/5)*3))		// avoid dichrotic noise by waiting 3/5 of last IBI
-					{       
-	        			if (Signal < T){                        // T is the trough
-	            			T = Signal;                         // keep track of lowest point in pulse wave 
-	         			}
-	       			}
-	      
-	    			if(Signal > thresh && Signal > P)          // thresh condition helps avoid noise
-	        		{
-						P = Signal;                             // P is the peak
-	        			//FPeak[i] = true;
-	       			}                                        // keep track of highest point in pulse wave
-	    
-					
-					
-	  				//  NOW IT'S TIME TO LOOK FOR THE HEART BEAT
-	  				// signal surges up in value every time there is a pulse
-					if (N > 250)                                   // avoid high frequency noise
-	  				{
-	  					
-	  					if((Signal> thresh) && (Pulse == false)) {
-	  					
-	  						FPeak[i] = true;
-	  					}
-	  					
-	  				
-						if ( (Signal > thresh) && (Pulse == false) && (N > (IBI/5)*3) )
-	  					{
-	    					Pulse = true;                               	// set the Pulse flag when we think there is a pulse
-	    					
-	    					FOutput[i] = true;
-	  						IBI = (sampleCounter - lastBeatTime);      		// measure time between beats in mS
-	    					lastBeatTime = sampleCounter;               	// keep track of time for next pulse
-	         
-	         				if(firstBeat){                         			// if it's the first time we found a beat, if firstBeat == TRUE
-	             				firstBeat = false;                 			// clear firstBeat flag
-	             				//return;                            			// IBI value is unreliable so discard it
-	            				//FPeak[i] = true;
-	         					
-	         				}
-	         				else 
-	  						{
-	  					
-		  						if(secondBeat){                        			// if this is the second beat, if secondBeat == TRUE
-		            				secondBeat = false;                 		// clear secondBeat flag
-		               				for(int j=0; j<=9; j++){         			// seed the running total to get a realisitic BPM at startup
-		                    			rate[j] = IBI;                      
-		                    		}
-		  							FPeak[i] = true;
-		            			}
-		  						
-		  						// keep a running total of the last 10 IBI values
-		    					ulong runningTotal = 0;                   		// clear the runningTotal variable, word    
-		
-							    for(int j=0; j<=8; j++){                		// shift data in the rate array
-							          rate[j] = rate[j+1];              		// and drop the oldest IBI value 
-							          runningTotal += (rate[j]);        // add up the 9 oldest IBI values
-							        }
-							        
-							    rate[9] = IBI;                          		// add the latest IBI to the rate array
-							    runningTotal += (rate[9]);              // add the latest IBI to runningTotal
-							    runningTotal /= 10;                     		// average the last 10 IBI values 
-							    BPM = (int)(60000/runningTotal);               		// how many beats can fit into a minute? that's BPM!
-							    QS = true;                              		// set Quantified Self flag 
-	  							FBPM[i] = BPM;
-							    // QS FLAG IS NOT CLEARED INSIDE THIS ISR
-	  							
-	  						}
-						}                       
+					if( (Signal < thresh) && (N > (IBI/5)*3))		// avoid dichrotic noise by waiting 3/5 of last IBI
+					{
+						if (Signal < T){                        // T is the trough
+							T = Signal;                         // keep track of lowest point in pulse wave
+						}
 					}
-	
-	  				if (Signal < thresh && Pulse == true)	   // when the values are going down, the beat is over
-					{     
-				      	FOutput[i] = false;
+					
+					if(Signal > thresh && Signal > P)          // thresh condition helps avoid noise
+					{
+						P = Signal;                             // P is the peak
+						//FPeak[i] = true;
+					}                                        // keep track of highest point in pulse wave
+					
+					
+					
+					//  NOW IT'S TIME TO LOOK FOR THE HEART BEAT
+					// signal surges up in value every time there is a pulse
+					if (N > 250)                                   // avoid high frequency noise
+					{
+						
+						if((Signal> thresh) && (Pulse == false)) {
+							
+							FPeak[i] = true;
+						}
+						
+						
+						if ( (Signal > thresh) && (Pulse == false) && (N > (IBI/5)*3) )
+						{
+							Pulse = true;                               	// set the Pulse flag when we think there is a pulse
+							
+							FOutput[i] = true;
+							IBI = (sampleCounter - lastBeatTime);      		// measure time between beats in mS
+							lastBeatTime = sampleCounter;               	// keep track of time for next pulse
+							
+							if(firstBeat){                         			// if it's the first time we found a beat, if firstBeat == TRUE
+								firstBeat = false;                 			// clear firstBeat flag
+								//return;                            			// IBI value is unreliable so discard it
+								//FPeak[i] = true;
+								
+							}
+							else
+							{
+								
+								if(secondBeat){                        			// if this is the second beat, if secondBeat == TRUE
+									secondBeat = false;                 		// clear secondBeat flag
+									for(int j=0; j<=9; j++){         			// seed the running total to get a realisitic BPM at startup
+										rate[j] = IBI;
+									}
+									FPeak[i] = true;
+								}
+								
+								// keep a running total of the last 10 IBI values
+								ulong runningTotal = 0;                   		// clear the runningTotal variable, word
+								
+								for(int j=0; j<=8; j++){                		// shift data in the rate array
+									rate[j] = rate[j+1];              		// and drop the oldest IBI value
+									runningTotal += (rate[j]);        // add up the 9 oldest IBI values
+								}
+								
+								rate[9] = IBI;                          		// add the latest IBI to the rate array
+								runningTotal += (rate[9]);              // add the latest IBI to runningTotal
+								runningTotal /= 10;                     		// average the last 10 IBI values
+								BPM = (int)(60000/runningTotal);               		// how many beats can fit into a minute? that's BPM!
+								QS = true;                              		// set Quantified Self flag
+								FBPM[i] = BPM;
+								// QS FLAG IS NOT CLEARED INSIDE THIS ISR
+								
+							}
+						}
+					}
+					
+					if (Signal < thresh && Pulse == true)	   // when the values are going down, the beat is over
+					{
+						FOutput[i] = false;
 						Pulse = false;                         // reset the Pulse flag so we can do it again
-				      	amp = P - T;                           // get amplitude of the pulse wave
-				      	thresh = amp/2 + T;                    // set thresh at 50% of the amplitude
-				      	P = thresh;                            // reset these for next time
-				      	T = thresh;
+						amp = P - T;                           // get amplitude of the pulse wave
+						thresh = amp/2 + T;                    // set thresh at 50% of the amplitude
+						P = thresh;                            // reset these for next time
+						T = thresh;
 						FPeak[i] = false;
-				    }
-	  
-				  	if (N > 2500){                             // if 2.5 seconds go by without a beat
-				      thresh = 0.5;                          // set thresh default
-				      P = 0.5;                               // set P default
-				      T = 0.5;                               // set T default
-				      lastBeatTime = sampleCounter;          // bring the lastBeatTime up to date        
-				      firstBeat = true;                      // set these to avoid noise
-				      secondBeat = true;                     // when we get the heartbeat back
-				  	  FPeak[i] = false;
-				    }
+					}
+					
+					if (N > 2500){                             // if 2.5 seconds go by without a beat
+						thresh = 0.5;                          // set thresh default
+						P = 0.5;                               // set P default
+						T = 0.5;                               // set T default
+						lastBeatTime = sampleCounter;          // bring the lastBeatTime up to date
+						firstBeat = true;                      // set these to avoid noise
+						secondBeat = true;                     // when we get the heartbeat back
+						FPeak[i] = false;
+					}
 					
 				}
 				
